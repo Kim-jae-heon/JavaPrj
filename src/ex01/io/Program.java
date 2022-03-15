82b2d0b829fea16c863ba97904f0751d70a180eb
Kim-jae-heon <77371295+Kim-jae-heon@users.noreply.github.com>
2022-03-15 15:28:48 +0900

    little modification
    조금 수정

--- src/ex01/io/Program.java
+++ src/ex01/io/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.write(3); //출력 버퍼에 3이라는 숫자를 채운 것. 다만 이것은 코드값.
+		System.out.write(51); //출력 버퍼에 3이라는 숫자를 채운 것. 다만 이것은 코드값.
 		System.out.flush();
 		
 		System.out.write('3'); //출력 버퍼에 3이라는 기호를 채운 것. 즉 컴퓨터는 52라는 숫자를 인식하게된다. Unicode상 3이라는 기호는 52라는 숫자를 나타내기 때문에
